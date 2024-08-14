3c77f1815c17767e05875188861842de9cc84c8b
Roaa K <ruaakhuldoon.26@gmail.com>
2024-08-14 22:19:07 +0300

    ADD HERO JSX FILE

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 function App() {
   
 
   return (
     <>
        <Navbar />
+       <Hero />
     </>
   )
 }

a463a7b063b27b380fe470e844161eec79eea230
Roaa K <ruaakhuldoon.26@gmail.com>
2024-08-14 23:15:41 +0300

    FIX IMAGES

--- components/Card.jsx
+++ components/Card.jsx
@@ -14,9 +14,9 @@ export default function Card(props) {
       {
         badgeText && <div className='card--badge'>{badgeText}</div>
       }
-      <img src={`./public/images/${props.coverImg}`} className='card--image'/>
+      <img src={`./images/${props.coverImg}`} className='card--image'/>
       <div className='card--stats'>
-        <img src='./public/images/star.png' className='card--star' />
+        <img src='./images/star.png' className='card--star' />
         <span>{props.stats.rating}</span>
         <span className='gray'>({props.stats.reviewCount}) • </span>
         <span className='gray'>{props.location}</span>

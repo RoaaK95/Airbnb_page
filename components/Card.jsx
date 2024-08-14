5c0f7f5d5f0a64baa1c1b76626b7b35eaeb7f08c
Roaa K <ruaakhuldoon.26@gmail.com>
2024-08-14 23:27:23 +0300

    debug

--- components/Card.jsx
+++ components/Card.jsx
@@ -13,8 +13,8 @@ export default function Card(props) {
     <div className='card'>
       {
         badgeText && <div className='card--badge'>{badgeText}</div>
-      }
-      <img src={`./images/${props.coverImg}`} className='card--image'/>
+      } 
+      <img src={`./public/images/${props.coverImg}`} className='card--image'/>
       <div className='card--stats'>
         <img src='./images/star.png' className='card--star' />
         <span>{props.stats.rating}</span>
